57d75f4e51c3dd4eed6004ce2724494dde12ab99
Samuel Carvajal <casato311409@gmail.com>
2023-09-14 10:57:19 -0500

    Dashboard - Backoffice Complete

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavigateComponent } from './navigate/navigate.component';
+import { LearnmoreComponent } from './learnmore/learnmore.component';
+
+const routes: Routes = [
+  {
+    path: '', component: NavigateComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: 'learnmore', component: LearnmoreComponent
+  },
+]
 
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
